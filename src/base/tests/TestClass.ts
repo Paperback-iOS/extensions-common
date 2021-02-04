e4f54165003d7b0b19a0862c79a03844a701c2d8
Conrad Weiser <vibrantclouds@protonmail.com>
2021-02-03 19:20:13 -0500

    Removal of Madara base

--- src/base/tests/TestClass.ts
+++
@@ -1,9 +0,0 @@
-import { LanguageCode } from "../../models";
-import { Madara } from "../Madara";
-
-export class TestClass extends Madara {
-    baseUrl: string = "https://www.webtoon.xyz"
-    languageCode: LanguageCode = LanguageCode.ENGLISH
-    sourceTraversalPathName: string = 'read'
-    homePage: string = 'webtoons'
-}
