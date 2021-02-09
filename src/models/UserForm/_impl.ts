95ef15ce1cb64989f90b4109322443e137b4293b
Conrad Weiser <vibrantclouds@protonmail.com>
2021-02-08 20:05:26 -0500

    UserForm - Explicit form types
    Rid of the enums

--- src/models/UserForm/_impl.ts
+++ src/models/UserForm/_impl.ts
@@ -1,4 +1,4 @@
-import {UserForm, FormObject} from '.'
+import {ComboFieldObject, PickerFieldObject, TextFieldObject, ToggleFieldObject, UserForm} from '.'
 
 const _global = global as any
 
@@ -6,6 +6,19 @@ _global.createUserForm = function (userForm: UserForm): UserForm {
     return userForm
 }
 
-_global.createFormObject = function (formObject: FormObject): FormObject {
-    return formObject
+_global.createTextFieldObject = function (textField: TextFieldObject): TextFieldObject {
+    return textField
 }
+
+_global.createToggleFieldObject = function (toggleField: ToggleFieldObject): ToggleFieldObject {
+    return toggleField
+}
+
+_global.createPickerFieldObject = function (pickerField: PickerFieldObject): PickerFieldObject {
+    return pickerField
+}
+
+_global.createComboFieldObject = function (comboField: ComboFieldObject): ComboFieldObject  {
+    return comboField
+}
+
--- src/models/UserForm/index.ts
+++ src/models/UserForm/index.ts
@@ -2,21 +2,33 @@ export interface UserForm {
     formElements: FormObject[]
 }
 
-export interface FormObject {
+interface FormObject {
     id: string
     userReadableTitle: string
-    type: FormType
-    metadata?: any
     userResponse?: any
 }
 
-export enum FormType {
-    TEXT_INPUT = "text",
-    TOGGLE_INPUT = "toggle",
-    PICKER_INPUT = "picker"
+export interface TextFieldObject extends FormObject {
+    placeholderText: string
 }
+
+export interface ToggleFieldObject extends FormObject {
+    // Blank - App consumes this for typing data
+}
+
+export interface PickerFieldObject extends FormObject {
+    values: string[]
+}
+
+export interface ComboFieldObject extends FormObject {
+    values: string[]
+}
+
 
 declare global {
     function createUserForm(userForm: UserForm): UserForm 
-    function createFormObject (formObject: FormObject): FormObject 
+    function createTextFieldObject (textField: TextFieldObject): TextFieldObject
+    function createToggleFieldObject (toggleField: ToggleFieldObject): ToggleFieldObject 
+    function createPickerFieldObject (pickerField: PickerFieldObject): PickerFieldObject 
+    function createComboFieldObject (comboField: ComboFieldObject): ComboFieldObject 
 }
