49cf2a6460a797b27832e876a7ad3b692a612bf5
Faizan Durrani <faizandurrani324@gmail.com>
2022-03-15 02:23:36 +0500

    added SearchFilter

--- src/base/Searchable.ts
+++ src/base/Searchable.ts
@@ -1,8 +1,9 @@
 import { PagedResults, SearchField, SearchRequest, TagSection, SearchFilter } from ".."
+import { SortOption } from "../models"
 import { Requestable } from "./Requestable"
 
 export interface Searchable extends Requestable {
-    getSearchResults(query: SearchRequest, metadata: unknown | undefined, searchFilter: SearchFilter | undefined): Promise<PagedResults>
+    getSearchResults(query: SearchRequest, metadata: unknown | undefined, sortOption: SortOption | undefined): Promise<PagedResults>
     
     getSearchFilter?(query: SearchRequest): Promise<SearchFilter>
     getSearchTags?(): Promise<TagSection[]>
